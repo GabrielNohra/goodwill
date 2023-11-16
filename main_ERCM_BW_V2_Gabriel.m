2e36702179e99c44030afdf1ee499ee7e36dad40
GabrielNohra <gabriel.nohra@gmail.com>
2023-11-16 12:59:27 +0100

    modified plot generation and storage variables v2

--- main_ERCM_BW_V2_Gabriel.m
+++ main_ERCM_BW_V2_Gabriel.m
@@ -162,9 +162,9 @@ facteur_tolerance_position = 10000.;
 % parametres de convergence sur l'identification materielle
 tolerance_LDC = 1e-4;
 %nb_iter_LDC_max = 5;
-nb_iter_LDC_max = 10;
+% nb_iter_LDC_max = 10;
 %nb_iter_LDC_max = 20;
-% nb_iter_LDC_max = 200;
+nb_iter_LDC_max = 200;
 
 % % seuil permettant de definir les elements de phase "pas assez deformes" pour pouvoir y identifier des proprietes
 % seuil_NRJ = 0.1; % 0 : pas de filtre
