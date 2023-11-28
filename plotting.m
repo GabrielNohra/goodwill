735a18b7898665784ea2c18f27292b6e7b4eae66
GabrielNohra <gabriel.nohra@gmail.com>
2023-11-28 15:55:40 +0100

    added new plotting ways

--- plotting.m
+++ plotting.m
@@ -21,6 +21,8 @@ function plotting(A,kappa,noise,dir)
     colorList = {color_1,color_2,color_3,color_4,...
                     color_5,color_6,color_7,color_8,color_9};
 
+    cd(dir{3});
+
     for i=1:size(A,1)
 
         gcf = figure;
@@ -37,7 +39,7 @@ function plotting(A,kappa,noise,dir)
         xl = xlabel('Number of iterations','interpreter','latex');
         yl = ylabel('$\mu$ [Pa]','interpreter','latex');
         lgd = legend({'Re $\left( \tilde{mu} \right)$','Im $\left( \tilde{mu} \right)$',...
-            'Re $\left( \mu \right)$','Im $\left( \mu \right)$'});
+            'Re $\left( \mu \right)$','Im $\left( \mu \right)$'},'interpreter','latex');
 
         [tl.FontSize, xl.FontSize, yl.FontSize] = deal(12);
         lgd.FontSize = 11;
