5e3ec081f0521f849022bf5dd50dd00ee9942ba1
GabrielNohra <gabriel.nohra@gmail.com>
2023-11-29 09:50:43 +0100

    fix plotting bugs

--- plotting.m
+++ plotting.m
@@ -43,8 +43,8 @@ function plotting(A,kappa,noise,dir)
         plot(1743*ones(1,size(A,2)),'-k');
         plot(174.3*ones(1,size(A,2)),'-k');
 
-        strTitle = sprintf('Material property $\\mu$ ($\\kappa$ = %0.2e, noise = %0.4f \\%%)',n(i),noise*100);
-        fileName = sprintf('results_(kappa=%0.2e).png',n(i)*100);
+        strTitle = sprintf('Material property $\\mu$ ($\\kappa$ = %0.2e, noise = %0.4f \\%%)',n(1,i),noise*100);
+        fileName = sprintf('results_(kappa=%0.2e).png',n(1,i)*100);
         tl = title(strTitle,'interpreter','latex');
         xl = xlabel('Number of iterations','interpreter','latex');
         yl = ylabel('$\mu$ [Pa]','interpreter','latex');
@@ -58,10 +58,6 @@ function plotting(A,kappa,noise,dir)
         saveas(gcf,fileName);
 
         close gcf;
-
-        if i ~= size(A,1)
-            n(1,i+1) = kappa / power(10,i);
-        end
 
     end
 
